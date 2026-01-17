fe07968d4527f4a8c6c6a031889781318c2edf6f
Vighnesh Tathare <v42069@yahoo.com>
2026-01-17 23:58:24 +0530

    added common logic for base code

---
+++ food-ordering-system/order-service/domain-module/order-domain-core/src/main/java/com/v/food/ordering/orderservice/entity/OrderItem.java
@@ -0,0 +1,82 @@
+package com.v.food.ordering.orderservice.entity;
+
+import com.v.food.ordering.common.entity.BaseEntity;
+import com.v.food.ordering.common.valueobjects.Money;
+import com.v.food.ordering.common.valueobjects.OrderId;
+import com.v.food.ordering.orderservice.valueobject.OrderItemId;
+import lombok.Getter;
+
+@Getter
+public class OrderItem extends BaseEntity<OrderItemId> {
+
+    private OrderId orderId;
+    private final Product product;
+    private final int quantity;
+    private final Money price;
+    private final Money subTotal;
+
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+        this.orderId = orderId;
+        super.setId(orderItemId);
+    }
+
+    boolean isPriceValid() {
+        return price.isGreaterThanZero() &&
+                price.equals(product.getPrice()) &&
+                price.multiply(quantity).equals(subTotal);
+    }
+
+    private OrderItem(Builder builder) {
+        super.setId(builder.orderItemId);
+        product = builder.product;
+        quantity = builder.quantity;
+        price = builder.price;
+        subTotal = builder.subTotal;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+
+    public static final class Builder {
+        private OrderItemId orderItemId;
+        private Product product;
+        private int quantity;
+        private Money price;
+        private Money subTotal;
+
+        private Builder() {
+        }
+
+        public Builder orderItemId(OrderItemId val) {
+            orderItemId = val;
+            return this;
+        }
+
+        public Builder product(Product val) {
+            product = val;
+            return this;
+        }
+
+        public Builder quantity(int val) {
+            quantity = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
+            return this;
+        }
+
+        public Builder subTotal(Money val) {
+            subTotal = val;
+            return this;
+        }
+
+        public OrderItem build() {
+            return new OrderItem(this);
+        }
+    }
+}
