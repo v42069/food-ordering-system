f2b54b40079ba477d9d361b63b75b2568a494654
Vighnesh Tathare <v42069@yahoo.com>
2026-01-22 23:37:46 +0530

    added core logic

--- food-ordering-system/order-service/domain-module/order-domain-core/src/main/java/com/v/food/ordering/orderservice/entity/Order.java
+++ food-ordering-system/order-service/domain-module/order-domain-core/src/main/java/com/v/food/ordering/orderservice/entity/Order.java
@@ -24,6 +24,118 @@ public class Order extends AggregateRoots<OrderId> {
 
     private OrderStatus orderStatus;
 
+    private Order(Builder builder) {
+        super.setId(builder.id);
+        customerId = builder.customerId;
+        restaurantId = builder.restaurantId;
+        deliveryAddress = builder.deliveryAddress;
+        price = builder.price;
+        orderItems = builder.orderItems;
+        trackingId = builder.trackingId;
+        orderStatus = builder.orderStatus;
+        failureMessages = builder.failureMessages;
+    }
+
+    public CustomerId getCustomerId() {
+        return customerId;
+    }
+
+
+    public RestaurantId getRestaurantId() {
+        return restaurantId;
+    }
+
+    public StreetAddress getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public TrackingId getTrackingId() {
+        return trackingId;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public List<String> getFailureMessages() {
+        return failureMessages;
+    }
+
     private List<String> failureMessages;
 
+    public static final class Builder {
+        private OrderId id;
+        private CustomerId customerId;
+        private RestaurantId restaurantId;
+        private StreetAddress deliveryAddress;
+        private Money price;
+        private List<OrderItem> orderItems;
+        private TrackingId trackingId;
+        private OrderStatus orderStatus;
+        private List<String> failureMessages;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder id(OrderId val) {
+            id = val;
+            return this;
+        }
+
+        public Builder customerId(CustomerId val) {
+            customerId = val;
+            return this;
+        }
+
+        public Builder restaurantId(RestaurantId val) {
+            restaurantId = val;
+            return this;
+        }
+
+        public Builder deliveryAddress(StreetAddress val) {
+            deliveryAddress = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
+            return this;
+        }
+
+        public Builder orderItems(List<OrderItem> val) {
+            orderItems = val;
+            return this;
+        }
+
+        public Builder trackingId(TrackingId val) {
+            trackingId = val;
+            return this;
+        }
+
+        public Builder orderStatus(OrderStatus val) {
+            orderStatus = val;
+            return this;
+        }
+
+        public Builder failureMessages(List<String> val) {
+            failureMessages = val;
+            return this;
+        }
+
+        public Order build() {
+            return new Order(this);
+        }
+    }
 }
