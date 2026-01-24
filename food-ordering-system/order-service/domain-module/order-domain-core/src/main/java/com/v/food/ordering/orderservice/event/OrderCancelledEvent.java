17a3f97da5aacc1a28bc8e5267fe63c8046d48ce
Vighnesh Tathare <v42069@yahoo.com>
2026-01-24 22:35:09 +0530

    added order event

---
+++ food-ordering-system/order-service/domain-module/order-domain-core/src/main/java/com/v/food/ordering/orderservice/event/OrderCancelledEvent.java
@@ -0,0 +1,13 @@
+package com.v.food.ordering.orderservice.event;
+
+
+import com.v.food.ordering.orderservice.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent extends OrderEvent {
+
+    public OrderCancelledEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
---
+++ food-ordering-system/order-service/domain-module/order-domain-core/src/main/java/com/v/food/ordering/orderservice/event/OrderEvent.java
@@ -0,0 +1,24 @@
+package com.v.food.ordering.orderservice.event;
+
+import com.v.food.ordering.common.event.DomainEvent;
+import com.v.food.ordering.orderservice.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public abstract class OrderEvent implements DomainEvent<Order> {
+    private final Order order;
+    private final ZonedDateTime createdAt;
+
+    public OrderEvent(Order order, ZonedDateTime createdAt) {
+        this.order = order;
+        this.createdAt = createdAt;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
